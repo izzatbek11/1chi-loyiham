98541aa7c5016864f32ef5ec7fc13c95b5506ffe
izzatbek11 <izzatbekxayitboyev420@gmail.com>
2024-10-28 13:13:17 +0500

    mashq2

--- main.py
+++ main.py
@@ -1,9 +1,56 @@
-def kopaytma(*sonlar):
-    natija = 1
-    for son in sonlar:
-        natija *= son
-    return natija
+class Shaxs:
+    def __init__(self, ism, familiya):
+        self.ism = ism
+        self.familiya = familiya
 
-# Misol uchun
-print(kopaytma(2, 3, 4))
-print(kopaytma(1, 5, 7, 2))
+    def get_info(self):
+        return f"Ism: {self.ism}, Familiya: {self.familiya}"
+
+class Professor(Shaxs):
+    def __init__(self, ism, familiya, mutaxassislik):
+        super().__init__(ism, familiya)
+        self.mutaxassislik = mutaxassislik
+
+    def get_info(self):
+        return f"Professor: {super().get_info()}, Mutaxassislik: {self.mutaxassislik}"
+
+class Foydalanuvchi(Shaxs):
+    def __init__(self, ism, familiya, email):
+        super().__init__(ism, familiya)
+        self.email = email
+
+    def get_info(self):
+        return f"Foydalanuvchi: {super().get_info()}, Email: {self.email}"
+
+class Sotuvchi(Shaxs):
+    def __init__(self, ism, familiya, mahsulot):
+        super().__init__(ism, familiya)
+        self.mahsulot = mahsulot
+
+    def get_info(self):
+        return f"Sotuvchi: {super().get_info()}, Mahsulot: {self.mahsulot}"
+
+class Mijoz(Shaxs):
+    def __init__(self, ism, familiya, telefon):
+        super().__init__(ism, familiya)
+        self.telefon = telefon
+
+    def get_info(self):
+        return f"Mijoz: {super().get_info()}, Telefon: {self.telefon}"
+
+class Admin(Foydalanuvchi):
+    def ban_user(self):
+        print("Foydalanuvchi bloklandi")
+
+professor = Professor("Ali", "Karimov", "Matematika")
+foydalanuvchi = Foydalanuvchi("Oyguloy", "Davlatova", "oyna@example.com")
+sotuvchi = Sotuvchi("Bobur", "Xolov", "Elektronika")
+mijoz = Mijoz("Jamila", "Saidova", "+9989283747474")
+admin = Admin("Jasur", "Rasulov", "jasur@gmail.com")
+
+print(professor.get_info())
+print(foydalanuvchi.get_info())
+print(sotuvchi.get_info())
+print(mijoz.get_info())
+admin.ban_user()
+print(admin.get_info())
